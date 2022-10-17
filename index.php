76feb85066bbb72ff8449f79e08ddee8a2854e26
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2022-10-18 01:02:43 +0900

    SEO対策

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 		<!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title>サイト作成・設計・3Dモデル・オンライン講座</title>
-
     </head>
+
+    <title>Field-UP|総合サービス</title>
 
     <body>
         <?php get_header("1");?>
--- single.php
+++ single.php
@@ -14,9 +14,9 @@ Template Name: ブログページ
 		<!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <title><?php the_title(); ?></title>
-
     </head>
+
+    <title><?php the_title(); ?></title>
 
     <body>
         <?php get_header("2");?>
