6ca203c92c51ea4f74ac61cebe91bf0fd8242602
keisuke-ueda <81659758+keisuke-ueda@users.noreply.github.com>
2022-10-18 01:19:26 +0900

    アーカイブページ修正

--- archive.php
+++ archive.php
@@ -8,13 +8,13 @@ Template Name: blog
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta description="" content="毎日の仕事や生活の中で代表やエンジニア、職人たちが感じたことや出来事を掲載していきます。他愛のない内容のものもや、役立つ情報などを掲載していきます。">
 		<!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     </head>
 
-    <title><?php the_title(); ?></title>
+    <title><?php the_title(); ?>|Field-UP</title>
 
     <body class="body">
         <?php get_header("2");?>
